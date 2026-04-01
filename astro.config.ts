b779c92be0e982284f2f027022c3931797a29fca
ast-ak47 <s2511142@u.tsukuba.ac.jp>
2026-04-02 06:32:26 +0900

    First Commit

---
+++ astro.config.ts
@@ -0,0 +1,17 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+export default defineConfig({
+  vite: {
+        server: {
+            watch: {
+                usePolling: true,
+            },
+        },
+  },
+  image: {
+    responsiveStyles: true,
+  },
+  site: "https://www.ak47.work"
+});
---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "Astro.js development environment";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        devShells.default =
+          with pkgs;
+          mkShell {
+            buildInputs = with pkgs; [
+              nodejs
+              nodePackages.pnpm
+            ];
+          };
+      }
+    );
+}
